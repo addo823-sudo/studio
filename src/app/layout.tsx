86e87a1abc57f30af5b6319914259bdb996f088e
Adrian Dorado Gallardo <adriaan0220@gmail.com>
2026-03-03 00:50:03 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="ca" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
